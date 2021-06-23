fc68d19ada26f68b63f7b66209ee3a8b065e5300
KshitijJainM <2018ise_kshitijjainm@nie.ac.in>
2021-06-23 09:10:31 +0530

    Add Home Screen

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import "./App.css";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import {
+	BrowserRouter as Router,
+	Routes,
+	Route,
+	Switch,
+} from "react-router-dom";
 import Login from "./components/Login";
 import Header from "./components/Header";
 function App() {
